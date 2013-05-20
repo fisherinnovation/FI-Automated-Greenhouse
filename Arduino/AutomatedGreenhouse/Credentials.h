dbeb487e6515e55879836b6f352ab9ffe9665ecb
Matt Fisher <fisher.matt@gmail.com>
2013-05-19 23:34:39 -0400

    Removing old Arduino setup.

--- Arduino/AutomatedGreenhouse/Credentials.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CREDENTIALS_H__
-#define __CREDENTIALS_H__
-
-// Wifi parameters
-char passphrase[] = "";
-char ssid[] = "";
-char ntp_server[] = "";
-
-#endif
--- Arduino/libraries/WiFlySerial/Examples/WebTimeA1/Credentials.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CREDENTIALS_H__
-#define __CREDENTIALS_H__
-
-// Wifi parameters
-char passphrase[] = "YourSecretPassPhrase123";
-char ssid[] = "YourSSID";
-char ntp_server[] = "nist1-la.ustiming.org"; // or your favorite ntp server.
-
-#endif
